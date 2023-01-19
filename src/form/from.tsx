712c1066d91995e4c6aec19086ca67070a1769e7
JOAO VITHOR GOMES VIEIRA <joaovithor.gv@gmail.com>
2023-01-19 09:40:55 -0300

    Purple is completed

--- src/form/from.tsx
+++ src/form/from.tsx
@@ -14,7 +14,15 @@ type Props_Forms = {
   className?: string;
 };
 
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
+
 export function Form(props: Props_Forms) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [email, setEmal] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="all_page">
       <form className={`grid-form ${props.className}`}>
@@ -23,33 +31,89 @@ export function Form(props: Props_Forms) {
         {props.user?.label ? (
           props.user.inverted ? (
             <div className={`grid-input ${props.user?.className}`}>
-              <input type="text" placeholder={props.user.placeholder} />
+              <input
+                className={`${props.user?.className}  ${
+                  email ? "input_filled" : ""
+                }`}
+                type="text"
+                onChange={(e) => setEmal(e.target.value)}
+                placeholder={props.user.placeholder}
+              />
               <label htmlFor="">{props.user.label}</label>
             </div>
           ) : (
             <div className={`grid-input ${props.user?.className}`}>
               <label htmlFor="">{props.user.label}</label>
-              <input type="text" placeholder={props.user.placeholder} />
+              <input
+                className={`${props.user?.className}  ${
+                  email ? "input_filled" : ""
+                }`}
+                type="text"
+                onChange={(e) => setEmal(e.target.value)}
+                placeholder={props.user.placeholder}
+              />
             </div>
           )
         ) : (
-          <input className={props.user?.className} type="text" />
+          <input
+            className={`${props.user?.className}  ${
+              email ? "input_filled" : ""
+            }`}
+            onChange={(e) => setEmal(e.target.value)}
+            type="text"
+            placeholder={props.user?.placeholder}
+          />
         )}
 
         {props.password?.label ? (
           props.password.inverted ? (
             <div className={`grid-input ${props.password?.className}`}>
-              <input type="text" placeholder={props.password.placeholder} />
+              <input
+                className={`${props.password?.className}  ${
+                  password ? "input_filled" : ""
+                }`}
+                type={showPassword ? "text" : "password"}
+                placeholder={props.password.placeholder}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <label htmlFor="">{props.password.label}</label>
             </div>
           ) : (
             <div className={`grid-input ${props.password?.className}`}>
               <label htmlFor="">{props.password.label}</label>
-              <input type="text" placeholder={props.password.placeholder} />
+              <input
+                className={`${props.password?.className}  ${
+                  password ? "input_filled" : ""
+                }`}
+                type={showPassword ? "text" : "password"}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder={props.password.placeholder}
+              />
             </div>
           )
         ) : (
-          <input type="password" className={props.password?.className} />
+          <input
+            className={`${props.password?.className}  ${
+              password ? "input_filled" : ""
+            }`}
+            onChange={(e) => setPassword(e.target.value)}
+            type={showPassword ? "text" : "password"}
+            placeholder={props.password?.placeholder}
+          />
+        )}
+
+        {showPassword ? (
+          <AiFillEyeInvisible
+            className="pass_icon"
+            size={20}
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        ) : (
+          <AiFillEye
+            className="pass_icon"
+            size={20}
+            onClick={() => setShowPassword(!showPassword)}
+          />
         )}
 
         <button className={props.button?.className}>
