5377d010edb6f4d25be55161738ead58df688327
JOAO VITHOR GOMES VIEIRA <joaovithor.gv@gmail.com>
2023-01-23 10:34:06 -0300

    routes

--- src/router.tsx
+++ src/router.tsx
@@ -6,9 +6,9 @@ import { Purple } from "./logins/purple";
 export function Router(){
     return(
         <Routes> 
-            <Route path={'/'} element={<Home />} />
-            <Route path={'/home'} element={<Home />} />
-            <Route path='/' element={<Purple/>} />
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            {/* <Route path='/' element={<Purple/>} /> */}
             <Route path='/purple' element={<Purple/>} />
             <Route path='/event/lesson/:slug' element={''} />
         </Routes>
